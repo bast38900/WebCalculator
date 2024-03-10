86aecc9d642c052dcf90e53529e87979b8e3b963
Bast38900 <bast38900@edu.ucl.dk>
2024-03-10 14:15:26 +0100

    all test created

--- WebCalculatorTests/MathOperatorTest.cs
+++ WebCalculatorTests/MathOperatorTest.cs
@@ -7,12 +7,60 @@ namespace WebCalculatorTests
         [Fact]
         public void AddNumbersTest()
         {
+            // Arrange
             var mathOperator = new MathOperator
             {
                 num1 = "1",
                 num2 = "2"
             };
+            // Act
             mathOperator.AddNumbers();
+            // Assert
+            Assert.Equal("3", mathOperator.finalresult);
+        }
+
+        [Fact]
+        public void SubtractNumbersTest()
+        {
+            // Arrange
+            var mathOperator = new MathOperator
+            {
+                num1 = "3",
+                num2 = "2"
+            };
+            // Act
+            mathOperator.SubtractNumbers();
+            // Assert
+            Assert.Equal("1", mathOperator.finalresult);
+        }
+
+        [Fact]
+        public void MultiplyNumbersTest()
+        {
+            // Arrange
+            var mathOperator = new MathOperator
+            {
+                num1 = "3",
+                num2 = "2"
+            };
+            // Act
+            mathOperator.MultiplyNumbers();
+            // Assert
+            Assert.Equal("6", mathOperator.finalresult);
+        }
+
+        [Fact]
+        public void DivideNumbersTest()
+        {
+            // Arrange
+            var mathOperator = new MathOperator
+            {
+                num1 = "6",
+                num2 = "2"
+            };
+            // Act
+            mathOperator.DivideNumbers();
+            // Assert
             Assert.Equal("3", mathOperator.finalresult);
         }
     }
